867b713600c9145f75acbafaf14df2a302c2f4c3
izadraalva <izadraalva@educacion.navarra.es>
2026-02-24 13:08:15 +0100

    Primer dia
    Actualización_Servidor_Base

---
+++ LigaVolei_Freemarker/LigaVolei_Freemarker/src/main/java/edu/masanz/LigaVolei/Main.java
@@ -0,0 +1,38 @@
+package edu.masanz.LigaVolei;
+
+import edu.masanz.LigaVolei.controller.NotaController;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinFreemarker;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.info("ARRANCANDO APLICACION");
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("public");
+            config.fileRenderer(new JavalinFreemarker());
+        }).start(8080);
+
+
+        app.get("/", NotaController::servirIndice);
+        app.get("/lista-notas", NotaController::servirLista);
+        app.get("/nota/{id}", NotaController::servirNota);
+        app.get("/crea-nota", NotaController::servirCrearNota);
+        app.post("/crea-nota", NotaController::crearNota);
+        app.get("/edita-nota/{id}", NotaController::servirEditarNota);
+        app.post("/edita-nota/{id}", NotaController::editarNota);
+        app.get("/elimina-nota/{id}", NotaController::servirEliminarNota);
+        app.post("/elimina-nota/{id}", NotaController::eliminarNota);
+        app.get("/error", NotaController::servirError);
+
+    }
+
+}
