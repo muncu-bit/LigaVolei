37836142fcf1cf7d99b1c0bd845d78f873adbe56
Vero <lpenaragon@educacion.navarra.es>
2026-02-25 08:35:04 +0100

    creación de base de datos de usuarios y contraseña, e inserciones

---
+++ doc/sql/insert.sql
@@ -0,0 +1,4 @@
+insert into usuarios values("Admin","Admin");
+insert into usuarios values("Vero","1234");
+insert into usuarios values("Iker","2345");
+insert into usuarios values("Maksim","3456");
