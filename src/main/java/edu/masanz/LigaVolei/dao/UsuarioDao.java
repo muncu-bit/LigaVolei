e106977cb917a319060bcccbeeadd180d240dff5
Veronica2213 <lpenaragon@educacion.navarra.es>
2026-02-27 09:37:46 +0100

    ftl con css e imagenes

--- src/main/java/edu/masanz/LigaVolei/dao/UsuarioDao.java
+++ src/main/java/edu/masanz/LigaVolei/dao/UsuarioDao.java
@@ -6,10 +6,10 @@ import org.eclipse.jetty.server.Authentication;
 
 public class UsuarioDao {
 
-    public static Usuariocrear userinicio(String nombre, String contraseña) {
+    public static Usuariocrear userinicio(String nombre, String contrasena) {
 
         String sql = "SELECT id, nombre, contraseña FROM usuarios WHERE nombre = ? AND contraseña = ?";
-        Object[] usuario = {nombre, contraseña};
+        Object[] usuario = {nombre, contrasena};
        Object [][] usuarioregistrado = ConnectionManager.ejecutarSelectSQL(sql,usuario);
 
 
@@ -17,7 +17,7 @@ public class UsuarioDao {
 
             int id = (int) usuarioregistrado[0][0];
 
-            Usuariocrear ejemplo = new Usuariocrear(nombre, contraseña, id);
+            Usuariocrear ejemplo = new Usuariocrear(nombre, contrasena, id);
 
                     return ejemplo;
         }
